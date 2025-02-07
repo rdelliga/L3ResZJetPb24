92106480e15a75bf6e3e51fce52b2787aa9c411f
Laura Martikainen <laura.martikainen@cern.ch>
2025-02-07 14:25:00 +0100

    Print JER SF in txt

--- JER/JERSF_printtxt.C
+++ JER/JERSF_printtxt.C
@@ -4,11 +4,28 @@ void JERSF_printtxt(string filein = "JERSFs_fromfits.root")  {
 
   auto file = new TFile(filein.c_str(),"READ");
   auto SFs = (TH1D*)file->Get("SF");
-  cout << " { 2 JetEta JetPt 0 None ScaleFactor } " << endl;
+  string header = "{ 2 JetEta JetPt 0 None ScaleFactor }";
+  string filename = "JERSF.txt";
+  float minpt = 0, maxpt = 7000;
+  
+  cout << header.c_str() << endl;
 
+  ofstream txtfile;
+  txtfile.open(filename.c_str());
+
+  std::cout << "Producing txt file " << filename.c_str() << " with header " << header.c_str() << std::endl;
+  txtfile << header.c_str() << std::endl;
+
+  
   // Neg eta
-  for (int i = 1; i < SFs->GetXaxis()->GetNbins()-1; ++i) {
-    cout << SFs->GetBinLowEdge(i) << " " <<  SFs->GetBinLowEdge(i+1)  << " 0 1000 3 " << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << endl;
+  for (int i = SFs->GetXaxis()->GetNbins()-1; i > 1 ; i--) {
+    cout << -SFs->GetBinLowEdge(i) << " " <<  (i == 1 ? -SFs->GetBinLowEdge(i-1) : SFs->GetBinLowEdge(i-1))  << " " << minpt << " " << maxpt << " 3 " << SFs->GetBinContent(i-1) << " "  << SFs->GetBinContent(i-1) << " "  << SFs->GetBinContent(i-1) << endl;
+    txtfile << -SFs->GetBinLowEdge(i) << " " <<  (i == 1 ? -SFs->GetBinLowEdge(i-1) : SFs->GetBinLowEdge(i-1))  << " " << minpt << " " << maxpt << " 3 " << SFs->GetBinContent(i-1) << " "  << SFs->GetBinContent(i-1) << " "  << SFs->GetBinContent(i-1) << endl;
+  }
+
+   for (int i = 1; i < SFs->GetXaxis()->GetNbins()-1; ++i) {
+    cout << SFs->GetBinLowEdge(i) << " " <<  SFs->GetBinLowEdge(i+1)  << " " << minpt << " " << maxpt << " 3 " << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << endl;
+    txtfile << SFs->GetBinLowEdge(i) << " " <<  SFs->GetBinLowEdge(i+1)  << " " << minpt << " " << maxpt << " 3 " << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << " "  << SFs->GetBinContent(i) << endl;
   }
  
 }
