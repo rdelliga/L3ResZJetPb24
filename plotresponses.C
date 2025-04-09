13a09e605f0db112cb398090025d4955ac5f63af
Laura Martikainen <laura.martikainen@cern.ch>
2025-04-09 16:45:34 +0200

    Response plot updates

--- plotresponses.C
+++ plotresponses.C
@@ -1,13 +1,14 @@
 
 //void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_HP_PFTRIG_nojetid_a3.root", string plottag = "PFTRIG_nojetid") {
-//void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_HP_PFTRIG_jetid_a3.root", string plottag = "PFTRIG_jetid") {
+void plotresponses( string input = "RERUNALL_combinedbins/L2residuals_pbpbreco_rerunall_HP_jetid.root", string plottag = "PFTRIG_jetid") {
 //void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_HP_CALOTRIG_nojetid_a3.root", string plottag = "CALOTRIG_nojetid") {
 //void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_HP_CALOTRIG_jetid_a3.root", string plottag = "CALOTRIG_jetid") {
-void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_zerobias_nojetid.root", string plottag = "ZB_nojetid") {
+//void plotresponses( string input = "RERUNALL_combinedbins/L2residuals_pbpbreco_rerunall_zerobias_jetid.root", string plottag = "ZB_jetid") {
 
   float h1 = -1.0, h2 = 0.0, eta1 = -5.2, eta2 = 5.2;
   
-  float rmax = 1.2, rmin = 0.75; // For initial checks
+  //float rmax = 1.1, rmin = 0.85; // For initial checks
+  float rmax = 1.3, rmin = 0.85; // For initial checks
   // float rmax = 1.07, rmin = 0.93; // For closure to get better zoom
 
   float etalimit[] = {3.0, 3.0, 2.964-0.08, 2.5, 1.93+0.12};// for shady box, do manually before figuring out something smart
@@ -23,9 +24,11 @@ void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_zerob
   TFile *file = new TFile(input.c_str(),"READ");
 
   float alpha = 0.3;
-  int pts[] = {15, 25, 55, 80, 120, 170, 1000}; // Temporary
+// int pts[] = {15, 25, 55, 80, 120, 170, 1000}; // Temporary
+int pts[] = {15, 25, 80, 120, 1000}; // Temporary
 
-  for (int i = 0; i < 6; ++i) {
+// for (int i = 0; i < 6; ++i) {
+for (int i = 0; i < 4; ++i) {
     int pt1 = pts[i];
     int pt2= pts[i+1];
     string namelabel = Form("pbpb_%dto%dalpha%.1f_%s",pt1,pt2,alpha,plottag.c_str());
@@ -36,7 +39,7 @@ void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_zerob
     //  mc->GetXaxis()->SetRangeUser(0,2.5);
     //  data->GetXaxis()->SetRangeUser(0,2.5);
     
-    auto leg = new TLegend(0.57,0.4,0.85,0.5); //  x, y, x, y
+    auto leg = new TLegend(0.67,0.7,0.85,0.8); //  x, y, x, y
     leg->SetTextSize(0.03);
     leg->SetBorderSize(0);
     leg->SetFillStyle(0);
@@ -46,6 +49,7 @@ void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_zerob
     TCanvas *c1 = new TCanvas("c1","c1",800,600);
 
     mc->GetXaxis()->SetTitle("|#eta_{probe}|");
+    mc->GetYaxis()->SetTitle("Response");
     auto rp = new TRatioPlot(mc, data);
     
     rp->GetLowYaxis()->SetNdivisions(505);
@@ -67,10 +71,10 @@ void plotresponses( string input = "RERUNALL/L2residuals_pbpbreco_rerunall_zerob
 
     auto txt = new TLatex();
     txt->SetTextSize(0.03);
-    txt->DrawLatex( 0.2, 0.4, Form("%d < p_{T} < %d",pt1,pt2));
+    txt->DrawLatex( 0.2, 0.4, Form("%d < p_{T,avg} < %d",pt1,pt2));
     txt->DrawLatex( 0.2, 0.45, Form("#alpha < %.1f",alpha));
   
-    c1->Print(Form("response_eta_%.1f_%.1f_%s.pdf",eta1,eta2,namelabel.c_str()));
+    c1->Print(Form("RERUNALL_combinedbins/response_eta_%.1f_%.1f_%s.pdf",eta1,eta2,namelabel.c_str()));
  
   }
 }
