34879a7974e2927c49b8a5d6ae26dcde5b39574d
Laura Martikainen <laura.martikainen@cern.ch>
2025-05-20 15:50:03 +0200

    Remove old scripts from old location

--- run.sh
+++
@@ -1,9 +0,0 @@
-
-
-root -l -q 'analyse.cc("localHP", "testIDsandtrig_AK4_PFTRIG1", false, false, false, false, false)'
-root -l -q 'analyse.cc("localHP", "testIDsandtrig_AK4_PFTRIG2", false, false, false, false, false)'
-
-
-
-
-
