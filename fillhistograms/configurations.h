285b5bd16e744319e8b1214bdfab790f4bfe895a
rdelliga <raffaele.delli.gatti@cern.ch>
2026-03-22 18:58:32 +0100

    Fixes to zjet analysis

--- fillhistograms/configurations.h
+++ fillhistograms/configurations.h
@@ -46,6 +46,8 @@ map<string,string> filenames = {
                    {"RERECOZB19", "/eos/user/l/lamartik/HIJECRERECO/zb19-2023-pbpbreco.root"},
 		   {"RERECOHP", "/eos/user/l/lamartik/HIJECRERECO/HP2023-pbpbreco.root"},
 		   //		   {"RERECOMC", "/eos/user/l/lamartik/HIJECRERECO/MC2023-pbpbreco.root"}
+                   {"ZJETMC_0", "root://cms-xrd-global.cern.ch//store/user/mnguyen/JEC/DYto2L-2Jets_MLL-50_TuneCP5_5p36TeV_amcatnloFXFX-pythia8/ZjetMadraph/260206_124018/0000/merged_HiForestMiniAOD.root"},
+                   {"ZJETDATA_0", "root://cms-xrd-global.cern.ch//store/user/mnguyen/JEC/PPRefSingleMuon0/SingleMuon0/merged_HiForestMiniAOD.root"},
                    {"ZJETMC", "/afs/cern.ch/user/r/rdelliga/L3_Res_ZJet_2024PbPb/CMSSW_15_1_0_patch3/residualanalysis/fillhistograms/DYto2Mu_MLL-50_PbPb24_HiForestMiniAOD_MC_1.root"},
 		   {"RERECOMC", "/eos/user/l/lamartik/Run3_MC_pbpbrereco_202509/MC_pbpbreco_allppGT.root"} ,
 		   {"PHOTONHP", "/eos/cms/store/group/phys_heavyions/bharikri/Forest/JetCalibrations/2024ppRef_HP0_part_from_Nick/HP0_PP2024_1.root"},
